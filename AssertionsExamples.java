dcbb999df58e6bacd7693c54c1117bde95aebaac
Naresh Chaurasia <johndoe@example.com>
2024-09-14 18:17:22 +0530

    Macbook Pro, Checkin Timestamp::Sat Sep 14 18:17:22 IST 2024

---
+++ AssertionsExamples.java
@@ -0,0 +1,43 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AssertionsExamples {
+
+    WebDriver driver;
+
+    @BeforeClass
+    void setup()
+    {
+        //Chrome browser
+        System.setProperty("webdriver.chrome.driver", "C:/Drivers/chromedriver_win32/chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.get("https://opensource-demo.orangehrmlive.com/");
+    }
+
+    @Test
+    void logoTest()
+    {
+        WebElement logo=driver.findElement(By.xpath("//*[@id=\"divLogo\"]/img"));
+        Assert.assertTrue(logo.isDisplayed(),"Logo not displayed");
+     }
+
+    @Test
+    void homePageTitleTest()
+    {
+        Assert.assertEquals("OrangeHRM",driver.getTitle(),"Title not matched");
+    }
+
+    @AfterClass
+    void tearDown()
+    {
+        driver.quit();
+    }
+
+
+}
